f7e153599d3bf24e45f08ddf7d19c6a61e453750
BbqGamer <korba.adam@gmail.com>
2023-06-04 21:04:50 +0200

    Add base project structure and deploy

---
+++ ui.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  skin = "black",
+  # Create dashboard header with image
+  dashboardHeader(title = "Stock market Analysis",
+    tags$li(a(href = "https://put.poznan.pl",
+              img(src = "logo.png",
+                  title = "PUT", height = "30px"),
+              style = "padding-top:10px; padding-bottom:10px;"),
+            class = "dropdown")),
+
+  ## Sidebar content
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("General Stock Market",
+        tabName = "general_dashboard", icon = icon("money-bill-trend-up")),
+
+      menuItem("Company Analysis",
+        tabName = "company_dashboard", icon = icon("building-user")),
+
+      menuItem("Stock Comparison",
+        tabName = "stock_comparison", icon = icon("code-compare")),
+
+      menuItem("Help",
+        tabName = "help", icon = icon("circle-info"))
+    )
+  ),
+
+  # ---------------------------------------
+  ## Body content
+  dashboardBody(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+    ),
+    tabItems(
+      # First tab content
+      tabItem(
+        tabName = "general_dashboard",
+        fluidRow(
+          box(plotOutput("plot1", height = 250)),
+          box(
+            title = "Controls",
+            sliderInput("slider", "Number of observations:", 1, 100, 50),
+          ),
+        )
+      ),
+
+      # Second tab content
+      tabItem(
+        tabName = "company_dashboard",
+        h2("Widgets tab content"),
+        fluidRow(
+          box(
+            title = "Controls",
+            sliderInput("slider", "Number of observations:", 1, 100, 50)
+          )
+        ),
+      ),
+
+      # third tab content
+      tabItem(
+        tabName = "stock_comparison"
+      ),
+
+      # fourth tab content
+      tabItem(
+        tabName = "help",
+      )
+    ),
+  )
+)
