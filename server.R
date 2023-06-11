e9ed7dd643290e8bc1ab754a2a3b690cf5dbf004
krzychuszala <krzychuszala@gmail.com>
2023-06-11 21:07:09 +0200

    second page plot with benchmarks

--- server.R
+++ server.R
@@ -23,9 +23,26 @@ transformFinancialData <- function(value) {
   return(value)
 }
 
+# funkcja do tego, żeby benchmarki mialy te same wiersze co akcje
+
+filter_common_dates <- function(df1, df2, df3) {
+  # Get the unique dates from each data frame
+  dates_df1 <- unique(df1$Date)
+  dates_df2 <- unique(df2$Date)
+  dates_df3 <- unique(df3$Date)
+  
+  # Find the common dates between df1 and df2
+  common_dates <- intersect(dates_df1, dates_df2)
+  common_dates <- intersect(common_dates, dates_df3)
+  
+  # Filter df3 to keep only the rows with common dates
+  df3_filtered <- df3[df3$Date %in% common_dates, ]
+  
+  # Return the filtered df3
+  return(df3_filtered)
+}
+
 server <- function(input, output, session) {
-  
-  
 
   # all separate csv for each company
   csv_directory <- "data/stocks"
@@ -34,6 +51,15 @@ server <- function(input, output, session) {
   for (csv_file in csv_files) {
     key <- tools::file_path_sans_ext(basename(csv_file))
     data_companies[[key]] <- read.csv(csv_file)
+    data_companies[[key]]$Date = as.Date(data_companies[[key]]$Date)
+    
+    for (i in 1:length(data_companies[[key]][, 1])) {
+      if (data_companies[[key]]$Close[i] >= data_companies[[key]]$Open[i]) {
+        data_companies[[key]]$direction[i] = "Increasing"
+      } else {
+        data_companies[[key]]$direction[i] = "Decreasing"
+      }
+    }
   }
   
   # widget z wyborem firmy 2page
@@ -42,27 +68,131 @@ server <- function(input, output, session) {
   companies$market_cap <- sapply(companies$market_cap, transformFinancialData)
   companies$market_cap <- as.numeric(companies$market_cap)
   
-  updateSelectInput(session, "ticker", choices = companies$stock)
+  nasdaq100 <- data.frame(read.csv("data/ndx_d.csv"))
+  spx500    <- data.frame(read.csv("data/spx_d.csv")) 
+  nasdaq100$Date <- as.Date(nasdaq100$Date)
+  spx500$Date <- as.Date(spx500$Date)
+  # class(nasdaq100$Date)
+  # class(data_companies$aapl$Date)
   
-  # table #3page
+  updateSelectInput(session, "ticker", choices = companies$stock)
+
+  # Events 
+  
+  # -----------------------------candleStick---------------------------------------
+  
+  # table
   output$ChoosingCompanytable <- DT::renderDataTable({
-    # dataset <- get(companies, "package:datasets")
-    companies[, c("index", "company_name", "market_capitalization", "stock", "sector", "industry")]
+    companies
     
     DT::datatable(
-      companies[, c("index", "company_name", "market_capitalization", "stock", "sector", "industry")],
+      companies[, c("index","company_name")],
       style = "bootstrap",
-      filter = "top",
+      # filter = "top",
       # callback = JS("limitSelectionTable"),
       rownames = FALSE,
       selection = list(mode = 'single', selected = c(1), target = 'row'), # selectable = c(-2, -3)),
       extensions = "Buttons",
       options = list(
         dom = 'Bfrtip',
-        buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
-        pageLength = 5  # Set the number of rows per 
+        # buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+        buttons = list(),
+        searching = FALSE,
+        ordering = FALSE,
+        pageLength = 5,  # Set the number of rows per 
+        lengthMenu = list(c(5, 10, 15), c('5', '10', '15'))
       )
     )
+  })
+  
+  observeEvent(input$ChoosingCompanytable_rows_selected, {
+    selected_indices <- input$ChoosingCompanytable_rows_selected
+    selected_row <- companies[selected_indices, ]
+    # print(selected_row)
+    if (selected_row$stock == "BRK.B") {
+      selected_row$stock <- gsub("\\.", "-", selected_row$stock)
+    }
+    selected_company <- data_companies[[tolower(selected_row$stock)]] # to juz jest .csv ze Stocks
+    
+    output$infoTable <- DT::renderDataTable({
+      selected_row
+      
+      DT::datatable(
+        selected_row,
+        style = "bootstrap",
+        rownames = TRUE,
+        selection = list(mode = 'none')
+      )
+      })
+    
+    output$candlePlot <- renderPlotly({
+      i <- list(line = list(color = '#17BECF'))
+      d <- list(line = list(color = '#7F7F7F'))
+      
+      
+      # print(selected_company)
+      # Plot candlestick chart
+      fig <- selected_company %>%
+        plot_ly(x = ~Date, type = "candlestick",
+                open = ~Open, close = ~Close,
+                high = ~High, low = ~Low, name = selected_company$company_name,
+                increasing = i, decreasing = d) 
+      fig <- fig %>% layout(yaxis = list(title = "Price"))
+      #fig <- fig %>% add_lines(x = ~Date, y = ~Open, line = list(color = 'black', width = 0.75), inherit = F)
+      # Create rangeselector buttons
+      rs <- list(visible = TRUE, x = 0.5, y = -0.1,
+                 xanchor = "center", yref = "paper",
+                 font = list(size = 9),
+                 buttons = list(
+                   list(count = 1,
+                        label = "RESET",
+                        step = "all"),
+                   list(count = 1,
+                        label = "1 YR",
+                        step = "year",
+                        stepmode = "backward"),
+                   list(count = 3,
+                        label = "3 MO",
+                        step = "month",
+                        stepmode = "backward"),
+                   list(count = 1,
+                        label = "1 MO",
+                        step = "month",
+                        stepmode = "backward")
+                 ))
+      
+      min_candle <- min(selected_company$Low) * 0.9  # Adjust the multiplier as desired
+      max_candle <- max(selected_company$High) * 1.1  # Adjust the multiplier as desired
+      
+      fig <- fig %>% layout(title = paste(selected_company$company_name), # , Sys.Date()),
+                            xaxis = list(rangeselector = rs, rangeslider = list(visible = F), 
+                                         title = list(
+                                           # text = "Date",
+                                           standoff = 4,  # Adjust the standoff value to move the caption down
+                                           font = list(size = 12)
+                                         )
+                            ), # list(showticklabels = FALSE), 
+                            yaxis = list(
+                              range = c(min_candle, max_candle),
+                              title = "Price"
+                            ),
+                            legend = list(orientation = "h", x = 0.5, y = 1,
+                                          xanchor = "center", yref = "paper",
+                                          font = list(size = 10),
+                                          bgcolor = "transparent"))
+      fig
+    })
+    
+    # -----------------------------candleStick---------------------------------------
+    
+    output$description <- renderPrint({
+      cat(paste("Company name:", selected_row$company_name, "\n",
+                "Market capitalization: ", selected_row$market_capitalization, "\n",
+                "Stock ticker: ", selected_row$stock, "\n",
+                "Sector: ", selected_row$sector, "\n",
+                "Industry: ", selected_row$industry))
+    })
+    
   })
   
   # table #3page
@@ -75,7 +205,7 @@ server <- function(input, output, session) {
       filter = "top",
       # callback = JS("limitSelectionTable"),
       rownames = FALSE,
-      selection = list(mode = 'multiple', selected = c(1), target = 'row'), # selectable = c(-2, -3)),
+      selection = list(mode = 'multiple', selected = c(1,2,3,4), target = 'row'), # selectable = c(-2, -3)),
       extensions = list("Buttons","Select"),
       options = list(
         dom = 'Bfrtip',
@@ -84,15 +214,13 @@ server <- function(input, output, session) {
       )
     )
   })
-
-  # Events 
   
   observeEvent(input$CompTable_rows_selected, {
     selected_indices <- input$CompTable_rows_selected
     selected_rows <- companies[selected_indices, ]
     
     output$selectedRows <- renderText({
-      paste("Selected Rows: ", paste(selected_rows$stock, collapse = ", "))
+      paste("Selected Stocks: ", paste(selected_rows$stock, collapse = ", "))
     })
     
     output$compBarChart <- renderPlotly({
@@ -101,7 +229,162 @@ server <- function(input, output, session) {
         x = ~stock,
         y = ~market_cap,
         type = "bar"
-      )
+      ) 
     })
   })
+  
+  # -----------------------benchmark scatter plot -----------------------
+  company_choices <- names(data_companies)
+  observe({
+    updateSelectInput(session, "company_name", choices = company_choices)
+  })
+  
+  # stock_list <- reactive({
+  #   names(data_companies)
+  # })
+  # 
+  
+  # # Filter the data frames to keep only the rows with common dates
+  # spx500 <- filter_common_dates(nasdaq100, data_companies$aapl, spx500)
+  # nasdaq100 <- filter_common_dates(spx500, data_companies$aapl, nasdaq100)
+  # filtered_date_company <- filter_common_dates(spx500, nasdaq100, data_companies$aapl)
+  
+  company_data <- list(
+    'Stock' = c(),
+    'S&P500' = spx500$Open,
+    'NASDAQ100' = nasdaq100$Open
+  )
+  
+  event_trigger <- reactive({
+    list(input$ChoosingCompanytable_rows_selected, input$price_type)
+  })
+
+  observeEvent(ignoreInit = TRUE, event_trigger(),{
+    selected_indices <- input$ChoosingCompanytable_rows_selected
+    selected_row <- companies[selected_indices, ]
+    
+    if (selected_row$stock == "BRK.B") {
+      selected_row$stock <- gsub("\\.", "-", selected_row$stock)
+    }
+    
+    selected_company <- data_companies[[tolower(selected_row$stock)]] # to juz jest .csv ze Stocks
+    
+    # Filter the data frames to keep only the rows with common dates
+    spx500 <- filter_common_dates(nasdaq100, selected_company, spx500)
+    nasdaq100 <- filter_common_dates(spx500, selected_company, nasdaq100)
+    filtered_date_company <- filter_common_dates(spx500, nasdaq100, selected_company)
+
+  
+    company_data$Stock = filtered_date_company[[input$price_type]]
+    company_data$`S&P500`= spx500[[input$price_type]]
+    company_data$NASDAQ100 = nasdaq100[[input$price_type]]
+
+    
+    output$stock_plot <- renderPlotly({
+      plot_data <- plot_ly()
+      
+      plot_data <- add_trace(plot_data,
+                             x = filtered_date_company$Date, # seq_along(company_data[[company]]), # spx500$Date,# 
+                             y = company_data[['Stock']],
+                             type = 'scatter',
+                             mode = 'lines',
+                             yaxis = "y1",
+                             name = selected_row$company_name)
+      plot_data <- add_trace(plot_data,
+                             x = filtered_date_company$Date, # seq_along(company_data[[company]]), # spx500$Date,# 
+                             y = company_data[['S&P500']],
+                             type = 'scatter',
+                             mode = 'lines',
+                             yaxis = "y2",
+                             name = "S&P500")
+      plot_data <- add_trace(plot_data,
+                             x = filtered_date_company$Date, # seq_along(company_data[[company]]), # spx500$Date,# 
+                             y = company_data[['NASDAQ100']],
+                             type = 'scatter',
+                             mode = 'lines',
+                             yaxis = "y2",
+                             name = "NASDAQ100")
+      
+      plot_data <- layout(plot_data,
+                          yaxis = list(title = "Stock Price"),
+                          yaxis2 = list(title = "Benchmark Price", overlaying = "y", side = "right"))
+      
+      plot_data
+    })
+    
+  })
+  
+  observeEvent(input$company_name, {
+    selected_company <- input$company_name
+    filtered_date_company <- filter_common_dates(spx500, nasdaq100, data_companies$aapl)
+    company_data$Stock = filtered_date_company$Open
+    
+    output$stock_plot <- renderPlotly({
+      plot_data <- plot_ly()
+      
+      # Add traces for each company
+      for (company in names(company_data)) {
+        plot_data <- add_trace(plot_data,
+                               x = spx500$Date,# seq_along(company_data[[company]]),
+                               y = company_data[[company]],
+                               type = 'scatter',
+                               mode = 'lines',
+                               name = company)
+      }
+      
+      plot_data
+    })
+  })
+  
+  update_plot <- function() {
+    plot_data <- plot_ly()
+    
+    # Add traces for each company
+    for (company in names(company_data)) {
+      plot_data <- add_trace(plot_data,
+                             x = spx500$Date,# seq_along(company_data[[company]]),
+                             y = company_data[[company]],
+                             type = 'scatter',
+                             mode = 'lines',
+                             name = company)
+    }
+    
+    # Update the stock plot
+    output$stock_plot <- renderPlotly({ plot_data })
+  }
+  # -----------------------benchmark scatter plot -----------------------
+
+  
+  # Add button event
+  observeEvent(input$add_button, {
+    company_name <- input$company_name
+    # stock_data <- input$stock_data
+    
+    if (company_name != "" ) { # && !is.na(stock_data)
+      company_data[[company_name]] <<- company_data2[[company_name]]
+      update_plot()
+      
+      # Reset input values
+      updateSelectInput(session, 'company_name', selected = "")
+      # updateNumericInput(session, 'stock_data', value = NA)
+    }
+  })
+  
+  # Delete button event
+  observeEvent(input$delete_button, {
+    company_name <- input$company_name
+    
+    if (company_name != "") {
+      company_data[[company_name]] <- NULL
+      company_data <<- company_data  # Use <<- to update the global variable
+      update_plot()
+      
+      # Reset input value
+      updateSelectInput(session, 'company_name', selected = "")
+    }
+  })
+  
+  # TEXTS
+  
+
 }
--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinydashboard)
+library(plotly)
 
 ui <- dashboardPage(
   skin = "black",
@@ -53,46 +54,132 @@ ui <- dashboardPage(
         )
       ),
 
-      # Second tab content
+
+        
+        #---------------beuzyteczne---------------------
+        
+        # h2("Widgets tab content"),
+        # fluidRow(
+        #   box(
+        #     title = "Controls",
+        #     sliderInput("slider", "Number of observations:", 1, 100, 50)
+        #   )
+        # ),
+        # fluidRow(
+        #   column(width = 1),
+        #   column(width = 3,
+        #          selectInput(
+        #            "ticker",
+        #            "Select an option:",
+        #            choices = NULL
+        #          )
+        #   )
+        # ),
+        
+        #---------------beuzyteczne---------------------
+        
+        #---------------moze zyteczne---------------------
+        
+        # fluidRow(
+        #   # Time range
+        #   radioButtons(
+        #     inputId = "period", 
+        #     label   = h4("Period"),
+        #     choices = list("1 month" = 1, "3 months" = 2, "6 months" = 3, "12 months" = 4, "YTD" = 5), 
+        #     selected = 4
+        #   ),
+        #   # Benchmark
+        
+        #---------------moze zyteczne---------------------
+      
       tabItem(
         tabName = "company_dashboard",
-        h2("Widgets tab content"),
-        fluidRow(
-          box(
-            title = "Controls",
-            sliderInput("slider", "Number of observations:", 1, 100, 50)
-          )
-        ),
-        fluidRow(
-          column(width = 1),
-          column(width = 3,
-                 selectInput(
-                   "ticker",
-                   "Select an option:",
-                   choices = NULL
-                 )
-          )
-        ),
-        fluidRow(
-          # Time range
-          radioButtons(
-            inputId = "period", 
-            label   = h4("Period"),
-            choices = list("1 month" = 1, "3 months" = 2, "6 months" = 3, "12      months" = 4, "YTD" = 5), 
-            selected = 4
-          ),
-          # Benchmark
-          radioButtons(
-            inputId  = "benchmark", 
-            label    = h4("Benchmark"),
-            choices  = list("SP500" = 1, "Nasdaq100" = 2,"None" = 3),
-            selected = 3)
-        ),
-        fluidRow(
-          DT::dataTableOutput("ChoosingCompanytable")
-        )
-
+            fluidRow(
+              column(
+                width = 12,
+                h2("Stock Price over time")
+              )
+            ),
+            fluidRow(
+              column(
+                width = 4,
+                DT::dataTableOutput("ChoosingCompanytable")
+              ),
+              column(
+                width = 8,
+                plotlyOutput("candlePlot")
+              )
+            ),
+            fluidRow(
+              column(
+                width = 8,
+                h2("Stock Price with benchmarks"),
+                plotlyOutput("stock_plot")
+              ),
+              column(
+                width = 4,
+                h3("Stock description"),
+                verbatimTextOutput("description"),
+                radioButtons(
+                  inputId  = "price_type",
+                  label    = h4("Price type:"),
+                  choices  = list("Open", "High","Low", "Close"),
+                  selected = "Open")
+                )
+              )
       ),
+      
+      # fluidRow(
+      #   column(
+      #     width = 8,
+      # 
+      #   ),
+      #   column(
+      #     width = 4,
+      #     dateRangeInput("xaxisRange", "X-axis range"),
+      #     plotlyOutput("infoTable")
+      #   )
+      # )
+ 
+      # Second tab content
+      # tabItem(tabName = "company_dashboard",
+      #   fluidPage(
+      #     fluidRow( # it sums to 12
+      #       column(width = 3,
+      #         DT::dataTableOutput("ChoosingCompanytable")
+      #       ),
+      #       column(width = 9,
+      #         plotlyOutput("candlePlot")     
+      #       )
+      #     ),
+      #   # fluidRow(
+      #     # column(width = 3,
+      #     #    # box(
+      #     #    #   # textOutput("desc")
+      #     #    # )
+      #     # ),
+      #     # column(width = 6,
+      #     #        dateRangeInput("xaxisRange", "X-axis range"),
+      #     #        # plotlyOutput("infoTable") 
+      #     #        ),
+      #     #  column(width = 4,
+      #     #         box(
+      #     #           title = "Company description:",
+      #     #           textOutput("desc")
+      #     #           # plotlyOutput("infoTable")
+      #     #         )
+      #     #  )
+      #   # ),
+      #   sidebarLayout(
+      #     sidebarPanel(
+      #       plotlyOutput("infoTable")
+      #     ),
+      #     mainPanel(
+      #       textOutput("Company description:")
+      #     )
+      #   )
+      # )
+      # ),
 
       # third tab content
       tabItem(
@@ -100,8 +187,8 @@ ui <- dashboardPage(
         fluidRow(
           DT::dataTableOutput("CompTable")
         ),
-        verbatimTextOutput("selectedRows"),
         fluidRow(
+          verbatimTextOutput("selectedRows"),
           plotlyOutput("compBarChart")
         )
       ),
@@ -109,7 +196,20 @@ ui <- dashboardPage(
       # fourth tab content
       tabItem(
         tabName = "help",
+        selectInput("company_name", "Select a company:", choices = c("")),
+        # sidebarLayout(
+        #   sidebarPanel(
+        #     selectInput("company_name", "Select to add/delete", choices = c("")),
+        #     actionButton("add_button", "Add"),
+        #     actionButton("delete_button", "Delete"),
+        #     hr(),
+        #     helpText("Note: Use the dropdown to select a company to add/delete.")
+        #   ),
+        #   mainPanel(
+        #     plotlyOutput("stock_plot")
+        #   )
+        # )
       )
-    ),
+    )
   )
 )
